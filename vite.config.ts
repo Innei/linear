17da81fbc59c1626ebdc65cc39aa25510cc87fc2
Innei <tukon479@gmail.com>
2024-11-19 00:15:30 +0800

    scan
    Signed-off-by: Innei <tukon479@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import MillionLint from '@million/lint'
 import reactRefresh from '@vitejs/plugin-react'
 import { RemoveWrapperFunction } from 'unplugin-ast/transformers'
 import AST from 'unplugin-ast/vite'
@@ -12,6 +13,7 @@ export default defineConfig({
   plugins: [
     reactRefresh(),
     tsconfigPaths(),
+    MillionLint.vite(),
     checker({
       typescript: true,
       enableBuild: true,
